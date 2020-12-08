cf2657cc0a8b8c2b5a2e590c7cc8870484a78e8a
Cristutiu Emanuel <cristutiumanu@gmail.com>
2020-12-08 22:52:34 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Pool2_Tablouri
+{
+    class Program
+    {
+
+        static void CateNumerePare()
+        {
+            // Se da o secventa de n numere. Sa se determine cate din ele sunt pare.
+
+            Console.WriteLine("Introduceti numerele din secvente: ");
+
+            int numarSecventa = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < numarSecventa; i++)
+            {
+                Console.ReadLine();
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
